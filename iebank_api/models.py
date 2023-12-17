24c0d1ba8d0169de135e183caa2d6e413085e27f
clorenzol <clorenzol.ieu2021@student.ie.edu>
2023-12-17 19:28:02 +0100

    backendchanges3

--- iebank_api/models.py
+++ iebank_api/models.py
@@ -8,10 +8,9 @@ class Account(db.Model):
     account_number = db.Column(db.String(20), nullable=False, unique=True)
     balance = db.Column(db.Float, nullable=False, default = 0.0)
     currency = db.Column(db.String(1), nullable=False, default="€")
-    country = db.Column(db.String(32), nullable=False, default="Spain")
     status = db.Column(db.String(10), nullable=False, default="Active")
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-   
+    country = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         return '<Event %r>' % self.account_number
